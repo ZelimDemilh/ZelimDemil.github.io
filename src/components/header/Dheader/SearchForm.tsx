4f40112ea5c7bdc5e08705fedcdc716f00900685
@zelimdemilh <zelemhanzzz@mail.ru>
2022-11-18 16:40:49 +0300

    разеделение на диплой и мобильную версию

---
+++ src/components/header/Dheader/SearchForm.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from "react";
+import style from "./header.module.css";
+
+interface SearchFormProps {
+  value: string,
+  setValue: (text:string) => void
+}
+
+
+const SearchForm: FC<SearchFormProps>= ({value, setValue}) => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default SearchForm;
---
+++ src/components/header/Dheader/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import style from "./header.module.css";
+
+const DHeader = () => {
+  return (
+    <div>
+      dheader
+    </div>
+  );
+};
+
+export default DHeader;
